f4d7f1212e51974234e677604d0b4fba1ca6b96b
bettyvng <betty.vg8@gmail.com>
2021-11-02 02:51:42 +0000

    use .push, count index, .length

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+//add flavor//
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//get firs flavour//
+console.log(iceCreamFlavours[0]);
+
+//get last flavour//
+console.log(iceCreamFlavours[5]);
+
+//total # of flavours//
+console.log(iceCreamFlavours.length);
