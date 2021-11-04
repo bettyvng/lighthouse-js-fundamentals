e4a0d3b8b7bb6cf6546eed911acce288a8645feb
bettyvng <betty.vg8@gmail.com>
2021-11-04 18:46:18 +0000

    assignment complete

---
+++ judgeVegetables.js
@@ -0,0 +1,26 @@
+const judgeVegetable = (vegetables, metric) => {
+  return   vegetables.reduce( (winner, contestant) =>
+                          contestant[metric] > winner[metric] ? contestant : winner).submitter
+}
+
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+]
+
+const metric = 'redness'
+
+console.log(judgeVegetable(vegetables, metric));
