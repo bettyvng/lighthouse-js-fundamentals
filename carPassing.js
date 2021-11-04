fd0f8976dc81a08f6738394f19b4cb765b58ebe9
bettyvng <betty.vg8@gmail.com>
2021-11-04 18:22:45 +0000

    assignment complete

---
+++ carPassing.js
@@ -0,0 +1,27 @@
+const carPassing = (cars, speed) => {
+  var time = Date.now()
+  var person = {
+    time: time,
+    speed: speed
+  }
+  cars.push(person)
+  return cars
+}
+const cars = [
+  {
+    time: 1568329654807,
+    speed: 40
+  },
+  {
+    time: 1568329821632,
+    speed: 42
+  },
+  {
+    time: 1568331115463,
+    speed: 35
+  }
+]
+
+const speed = 38
+
+console.log (carPassing(cars, speed))
